ca48a260ba0112093a54e98cb9de5620111d05ff
Casey Zimmer <czimmer@ramapo.edu>
2018-08-08 01:39:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function takeANumber(currentLine,newName)
   return ("Welcome, " + newName+ ". You are number "+currentLine.length+ " in line.");
   
 }
+
+function nowServing(currentLine)
+{
+  
+  if(currentLine.length > 0)
+    return currentLine.shift();
+  else
+    return ("There is nobody waiting to be served!");
+  
+  
+}
