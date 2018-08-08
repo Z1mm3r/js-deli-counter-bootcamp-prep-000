e88c70749ad06cf88fd8442cbeb422effaf995ac
Casey Zimmer <czimmer@ramapo.edu>
2018-08-08 01:37:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function takeANumber(currentLine,newName)
 {
   currentLine.push(newName);
   
-  return console.log("Welcome, " + newName+ ". You are number "+currentLine.length+ " in line.");
+  return ("Welcome, " + newName+ ". You are number "+currentLine.length+ " in line.");
   
 }
